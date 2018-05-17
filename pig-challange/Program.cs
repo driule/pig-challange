4600b8a4abd951e4c56d30642b88c3cfabde126c
Andrius Jaškauskas <jaskauskas.andrius@gmail.com>
2018-05-17 13:31:07 +0200

    Initial commit

---
+++ pig-challange/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pig_challange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to pig challange!");
+            Console.ReadLine();
+        }
+    }
+}
