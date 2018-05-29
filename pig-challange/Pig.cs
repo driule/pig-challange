cb1d612dd2a89e9ffa8059edc82a280edd0975fb
Andrius Jaškauskas <jaskauskas.andrius@gmail.com>
2018-05-29 12:55:11 +0200

    function renamed

--- pig-challange/Pig.cs
+++ pig-challange/Pig.cs
@@ -24,13 +24,13 @@ namespace pig_challange
             int[] newPosition = (positions.Count() > 0) ? positions[this.randomizer.Next(0, positions.Count() - 1)] : position;
 
             // analyse the gamestate and board to determine if the pig is capturable form its new position
-            this.DetermineCapturable(map, state, newPosition);
+            this.DetermineIsPigCapturable(map, state, newPosition);
 
             state.MoveAgent(AgentIdentifier.Pig, newPosition);
         }
 
         // the pig is capturable if two or fewer nearby squares are empty
-        private void DetermineCapturable(Map map, State state, int[] position)
+        private void DetermineIsPigCapturable(Map map, State state, int[] position)
         {
             state.IsPigCapturable = (map.GetAvailablePositions(position, state)).Count() <= 2;
         }
