26f0f17fb7b74153629f2cd782e74d40ea1bb250
Tobias van Driessel <tobiasvandriessel@startmail.com>
2018-06-07 14:51:24 +0200

    calculated new P(c)

--- pig-challange/Game.cs
+++ pig-challange/Game.cs
@@ -119,6 +119,10 @@ namespace pig_challenge
             //First call GetPMCConditionalPropabilities and GetCooperationPrior, then calculate for all m and all c: P(m|c) * P(c), put them in a temporary list. 
             //  Then calculate over all m and all c: P(c|m) = ( P(m|c) * P(c)) / SUM_C( P(m|i)*P(i) ). So only bottom half really needs to be calculated and 
             //  we use the precalculated ones from the list created.
+            List<float> PMCs = this.GetPMCConditionalProbabilities(map, state, id);
+            float cooperationProbability = state.GetCooperationProbability(id);
+
+            //Calculate P(m) by going over all SUM_C( P(m|i) * P(i) ) //TODO: smartly
 
             return new List<float>();
         }
@@ -131,6 +135,7 @@ namespace pig_challenge
         /// <returns></returns>
         public List<float> GetPMCConditionalProbabilities(Map map, State state, BasicAgent.AgentIdentifier id)
         {
+            //TODO, sort in order: up, right, down, left
             Position position = state.GetPosition(id);
 
             List<Position> availablePositions = map.GetAvailablePositions(position, state);
@@ -157,15 +162,58 @@ namespace pig_challenge
             return probabilities;
         }
 
-        private float GetCooperationPrior(State state)
+        private float CalculateCooperationPrior(State state, List<float> PCMs, BasicAgent.AgentIdentifier id)
         {
-            return 0.0f;
-
+            //WRONG return state.GetCooperationProbability(id);
             //Use conditional probability matrix from state to get the probability of cooperation from last turn. 
             // Or when it's the first turn, use the prior?
 
+            Position pos = state.GetPosition(id);
+            Position prevPos = state.GetPrevPosition(id);
 
+            //Switch based on previous movement, up = 0, right = 1, down = 2, left = 3
+            //For this, we do need a fixed size of the PMC list, so all moves need to be represented
+            return PCMs[this.GetMoveCode(pos, prevPos)];
+        }
 
+        /// <summary>
+        /// Return move code based on previous movement, up = 0, right = 1, down = 2, left = 3
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <param name="prevPos"></param>
+        /// <returns></returns>
+        private int GetMoveCode(Position pos, Position prevPos)
+        {
+            Position diff = pos - prevPos;
+
+            //Vertical movement
+            if (diff.X == 0)
+            {
+                //Up
+                if(diff.Y < 0)
+                {
+                    return 0;
+                }
+                //Down
+                else
+                {
+                    return 2;
+                }
+            }
+            //Horizontal movement
+            else
+            {
+                //Left
+                if(diff.X < 0)
+                {
+                    return 3;
+                }
+                //Right
+                else
+                {
+                    return 1;
+                }
+            }
         }
     }
 }
