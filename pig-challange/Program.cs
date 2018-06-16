36111225803764793090adbf8f3473ec98dc5ed0
Tobias van Driessel <tobiasvandriessel@startmail.com>
2018-06-16 19:12:54 +0200

    stufff

--- pig-challange/Program.cs
+++ pig-challange/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
 
 namespace pig_challenge
 {
@@ -12,11 +15,80 @@ namespace pig_challenge
             Console.WriteLine("Welcome to the pig challenge!");
             Console.WriteLine("Press Enter to start a new tournament.");
 
-            AgentConfiguration agentConfigurationA = new AgentConfiguration { alpha = 0.1f, beta = 0.2f, gamma = 0.3f, delta = 0.4f };
-            AgentConfiguration agentConfigurationB = new AgentConfiguration { alpha = 0.1f, beta = 0.2f, gamma = 0.3f, delta = 0.4f };
+            RunForRandomConfigurations();
 
-            Tournament tournament = new Tournament(NUM_GAMES, MAX_ITERATIONS);
-            tournament.Run(agentConfigurationA, agentConfigurationB);
+            Console.ReadLine();
+            
+        }
+
+        static void RunForRandomConfigurations()
+        {
+            var csv = new StringBuilder();
+            //Stopwatch stopwatch = new Stopwatch();
+
+            float[] valuesA = new float[4], valuesB = new float[4];
+
+            int count = 0;
+            for (float alphaA = 0.3f; alphaA < 1.0f; alphaA += 0.3f)
+                for (float alphaB = 0.3f; alphaB < 1.0f; alphaB += 0.3f)
+                    for (float betaA = 0.3f; betaA < 1.0f; betaA += 0.3f)
+                        for (float betaB = 0.3f; betaB < 1.0f; betaB += 0.3f)
+                            for (float gammaA = 0.3f; gammaA < 1.0f; gammaA += 0.3f)
+                                for (float gammaB = 0.3f; gammaB < 1.0f; gammaB += 0.3f)
+                                    for (float deltaA = 0.3f; deltaA < 1.0f; deltaA += 0.3f)
+                                        for (float deltaB = 0.3f; deltaB < 1.0f; deltaB += 0.3f)
+                                        {
+                                            count++;
+                                            if(count % 50 == 0)
+                                                Console.WriteLine($"Iteration {count}");
+                                            //stopwatch.Reset();
+                                            //stopwatch.Start();
+
+                                            valuesA[0] = alphaA;
+                                            valuesA[1] = betaA;
+                                            valuesA[2] = gammaA;
+                                            valuesA[3] = deltaA;
+
+                                            valuesB[0] = alphaB;
+                                            valuesB[1] = betaB;
+                                            valuesB[2] = gammaB;
+                                            valuesB[3] = deltaB;
+
+                                            valuesA = NormalizeValues(valuesA);
+                                            valuesB = NormalizeValues(valuesB);
+
+
+                                            AgentConfiguration agentConfigurationA = new AgentConfiguration { alpha = alphaA, beta = betaA, gamma = gammaA, delta = deltaA };
+                                            AgentConfiguration agentConfigurationB = new AgentConfiguration { alpha = alphaB, beta = betaB, gamma = gammaB, delta = deltaB };
+
+                                            Tournament tournament = new Tournament(NUM_GAMES, MAX_ITERATIONS);
+                                            csv.AppendLine($"Configuration A: {valuesA[0]}, {valuesA[1]}, {valuesA[2]}, {valuesA[3]} \n Configuration B: {valuesB[0]}, {valuesB[1]}, {valuesB[2]}, {valuesB[3]} ");
+                                            Result res = tournament.Run(agentConfigurationA, agentConfigurationB);
+                                            csv.AppendLine($"Total scores: \n Agent A: {res.scoreA}, Agent B: {res.scoreB}");
+
+                                            //stopwatch.Stop();
+                                            //Console.WriteLine("Ticks: " + stopwatch.ElapsedTicks +
+                                            //            " mS: " + stopwatch.ElapsedMilliseconds);
+                                            //Console.ReadLine();
+                                            //300 ms
+                                        }
+            File.WriteAllText("./results.csv", csv.ToString());
+
+
+
+
+        }
+
+        static float[] NormalizeValues(float[] values)
+        {
+            float sum = 0.0f;
+            for (int i = 0; i < 4; i++)
+                sum += values[i];
+
+            for (int i = 0; i < 4; i++)
+                values[i] = values[i] / sum;
+
+            return values;
         }
     }
 }
--- pig-challange/Tournament.cs
+++ pig-challange/Tournament.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 
 namespace pig_challenge
 {
+    struct Result
+    {
+        public int scoreA, scoreB;
+    }
+
     class Tournament
     {
         private List<int[]> scoreList;
@@ -21,7 +26,7 @@ namespace pig_challenge
             this.MaxIterations = maxIterations;
         }
 
-        public void Run(AgentConfiguration configurationA, AgentConfiguration configurationB)
+        public Result Run(AgentConfiguration configurationA, AgentConfiguration configurationB)
         {
             for (int i = 0; i < this.NumberOfGames; i++)
             {
@@ -30,16 +35,17 @@ namespace pig_challenge
                 this.scoreList.Add(new int[] { endState.ScoreAgentA, endState.ScoreAgentB });
             }
 
-            Console.WriteLine("Scores for the two agents:");
+            //Console.WriteLine("Scores for the two agents:");
             for (int i = 0; i < this.NumberOfGames; i++)
             {
-                Console.WriteLine($" Agent A: {this.scoreList[i][0]}, Agent B: {this.scoreList[i][1]}");
+                //Console.WriteLine($" Agent A: {this.scoreList[i][0]}, Agent B: {this.scoreList[i][1]}");
                 this.totalScoreAgentA += this.scoreList[i][0];
                 this.totalScoreAgentB += this.scoreList[i][1];
             }
-            Console.WriteLine($"Total scores: \n Agent A: {this.totalScoreAgentA}, Agent B: {this.totalScoreAgentB}");
+            //Console.WriteLine($"Total scores: \n Agent A: {this.totalScoreAgentA}, Agent B: {this.totalScoreAgentB}");
 
-            Console.ReadLine();
+            return new Result { scoreA = this.totalScoreAgentA, scoreB = this.totalScoreAgentB };
+            //Console.ReadLine();
         }
     }
 }
