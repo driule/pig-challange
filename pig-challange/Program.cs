95129856cd882cf731d4bf4052c2650e793f74a6
Andrius Jaškauskas <jaskauskas.andrius@gmail.com>
2018-06-26 15:18:15 +0200

    experiment with different initial guess

--- pig-challange/Program.cs
+++ pig-challange/Program.cs
@@ -13,6 +13,8 @@ namespace pig_challenge
         const int NUM_GAMES = 10;
         const int MAX_ITERATIONS = 25;
 
+        const int TOTAL_CONFIGURATIONS = 108900;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the pig challenge!");
@@ -34,31 +36,41 @@ namespace pig_challenge
             for (int alphaA = 0; alphaA <= 100; alphaA += step)
                 for (int betaA = 0; betaA <= 100; betaA += step)
                     for (int gammaA = 0; gammaA <= 100; gammaA += step)
-                        for (int deltaA = 0; deltaA <= 100; deltaA += step)
-                        {
-                            if (alphaA + betaA + gammaA + deltaA != 100)
-                                continue;
-                            for (int alphaB = 0; alphaB <= 100; alphaB += step)
-                                for (int betaB = 0; betaB <= 100; betaB += step)
-                                    for (int gammaB = 0; gammaB <= 100; gammaB += step)
-                                        for (int deltaB = 0; deltaB <= 100; deltaB += step)
-                                        {
-                                            if (alphaB + betaB + gammaB + deltaB != 100)
-                                                continue;
+                        //for (int deltaA = 0; deltaA <= 100; deltaA += step)
+                    {
+                        if (alphaA + betaA + gammaA != 100)
+                            continue;
+                        for (int alphaB = 0; alphaB <= 100; alphaB += step)
+                            for (int betaB = 0; betaB <= 100; betaB += step)
+                                for (int gammaB = 0; gammaB <= 100; gammaB += step)
+                                    //for (int deltaB = 0; deltaB <= 100; deltaB += step)
+                                {
+                                    if (alphaB + betaB + gammaB != 100)
+                                        continue;
 
+                                    int initialGuessStep = 25;
+                                    for (int initialGuessA = 0; initialGuessA <= 100; initialGuessA += initialGuessStep)
+                                    {
+                                        for (int initialGuessB = 0; initialGuessB <= 100; initialGuessB += initialGuessStep)
+                                        {
                                             floatArray[count, 0] = alphaA / 100.0f;
                                             floatArray[count, 1] = betaA / 100.0f;
                                             floatArray[count, 2] = gammaA / 100.0f;
-                                            floatArray[count, 3] = deltaA / 100.0f;
+                                            floatArray[count, 3] = initialGuessA / 100.0f;
 
                                             floatArray[count, 4] = alphaB / 100.0f;
                                             floatArray[count, 5] = betaB / 100.0f;
                                             floatArray[count, 6] = gammaB / 100.0f;
-                                            floatArray[count, 7] = deltaB / 100.0f;
+                                            floatArray[count, 7] = initialGuessB / 100.0f;
 
                                             count++;
                                         }
-                        }
+                                    }
+                                }
+                    }
+
+            //Console.WriteLine(count);
+            //Console.ReadLine();
         }
 
         static void RunDemo()
@@ -72,55 +84,60 @@ namespace pig_challenge
 
         static void RunForRandomConfigurations()
         {
-            float[,] floatArray = new float[81796, 8];
+            float[,] floatArray = new float[TOTAL_CONFIGURATIONS, 8];
             FillConfigurations(floatArray);
 
-            Result[] resultArray = new Result[81796];
+            Result[] resultArray = new Result[TOTAL_CONFIGURATIONS];
             var csv = new StringBuilder();
-
 
             //Stopwatch stopwatch = new Stopwatch();
 
             int count = 0;
-            Parallel.For(0, 4, id =>
-            {
-
-                for (int i = id * 20449; i < (id + 1) * 20449; i++)
+            Parallel.For(
+                0,
+                4,
+                id =>
                 {
-                    count++;
-                    if (count % 100 == 0)
-                        Console.WriteLine($"Iteration {count}");
-                    //stopwatch.Reset();
-                    //stopwatch.Start();
+
+                    for (int i = id * TOTAL_CONFIGURATIONS / 4; i < (id + 1) * TOTAL_CONFIGURATIONS / 4; i++)
+                    {
+                        count++;
+                        if (count % 100 == 0)
+                            Console.WriteLine($"Iteration {count}");
+                        //stopwatch.Reset();
+                        //stopwatch.Start();
 
 
-                    AgentConfiguration agentConfigurationA = new AgentConfiguration(floatArray[i, 0], floatArray[i, 1], floatArray[i, 2], floatArray[i, 3], 0.75f, 0.25f, 0.5f, true );
-                    AgentConfiguration agentConfigurationB = new AgentConfiguration(floatArray[i, 4], floatArray[i, 5], floatArray[i, 6], floatArray[i, 7], 0.75f, 0.25f, 0.5f, true );
+                        AgentConfiguration agentConfigurationA = new AgentConfiguration(floatArray[i, 0], floatArray[i, 1], floatArray[i, 2], 0.0f, 0.75f, 0.25f, floatArray[i, 3], true );
+                        AgentConfiguration agentConfigurationB = new AgentConfiguration(floatArray[i, 4], floatArray[i, 5], floatArray[i, 6], 0.0f, 0.75f, 0.25f, floatArray[i, 7], true );
 
-                    Tournament tournament = new Tournament(NUM_GAMES, MAX_ITERATIONS);
+                        Tournament tournament = new Tournament(NUM_GAMES, MAX_ITERATIONS);
 
-                    Result res = tournament.Run(agentConfigurationA, agentConfigurationB);
-                    resultArray[i] = res; ;
+                        Result res = tournament.Run(agentConfigurationA, agentConfigurationB);
+                        resultArray[i] = res; ;
 
-                    //stopwatch.Stop();
-                    //Console.WriteLine("Ticks: " + stopwatch.ElapsedTicks +
-                    //            " mS: " + stopwatch.ElapsedMilliseconds);
-                    //Console.ReadLine();
+                        //stopwatch.Stop();
+                        //Console.WriteLine("Ticks: " + stopwatch.ElapsedTicks +
+                        //            " mS: " + stopwatch.ElapsedMilliseconds);
+                        //Console.ReadLine();
+                    }
                 }
+            );
 
-            });
+            csv.AppendLine($"sep =\";\"");
 
-            for(int i = 0; i < 81796; i++)
+            for (int i = 0; i < TOTAL_CONFIGURATIONS; i++)
             {
-                csv.AppendLine($"Configuration A: {floatArray[i, 0]}, {floatArray[i, 1]}, {floatArray[i, 2]}, {floatArray[i, 3]} \n " +
-                       $"Configuration B: {floatArray[i, 4]}, {floatArray[i, 5]}, {floatArray[i, 6]}, {floatArray[i, 7]} ");
-                csv.AppendLine($"Total scores: \n Agent A: {resultArray[i].scoreA}, Agent B: {resultArray[i].scoreB}");
-                File.WriteAllText("./results.csv", csv.ToString());
-
-                //0_1_0_0;0_1_0_0;56787;4567;
+                csv.AppendLine(
+                    $"{floatArray[i, 0]}_{floatArray[i, 1]}_{floatArray[i, 2]}_{floatArray[i, 3]};" +
+                    $"{floatArray[i, 4]}_{floatArray[i, 5]}_{floatArray[i, 6]}_{floatArray[i, 7]};" +
+                    $"{resultArray[i].scoreA};{resultArray[i].scoreB};"
+                );
             }
 
+            File.WriteAllText("./results.csv", csv.ToString());
 
+            Console.WriteLine("Writing results done!");
         }
     }
 }
