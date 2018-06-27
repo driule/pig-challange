353cdf586d5e49563ee8e742a17272e89fff287f
Andrius Jaškauskas <jaskauskas.andrius@gmail.com>
2018-06-27 18:38:33 +0200

    redundand lib imports removed

--- pig-challange/Pig.cs
+++ pig-challange/Pig.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using RoyT.AStar;
 
 namespace pig_challenge
--- pig-challange/State.cs
+++ pig-challange/State.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using RoyT.AStar;
 
 namespace pig_challenge
